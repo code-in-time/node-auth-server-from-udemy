8f2b30d13b54678882b9dde4f361f6ba588768ce
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-03-23 17:35:26 +0200

    fix call back

--- models/user.js
+++ models/user.js
@@ -26,11 +26,10 @@ userSchema.pre('save', function(next) {
 
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
     bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
-        if (err) { throw (err); }
-        if (err) { return callback(err); }
+       // if (err) { return callback(err); }
+       // callback(null, isMatch);
     });
 
-    callback(null, isMatch);
 }
 
 // Create the model class
